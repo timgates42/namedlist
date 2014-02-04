a520426e416dff4b01b713efc38cc67d3c210caa
Eric V. Smith <eric@trueblade.com>
2014-02-04 13:29:58 -0500

    Release 1.0.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class PyTest(Command):
 
 
 setup(name='namedlist',
-      version='0.4',
+      version='1.0',
       url='https://bitbucket.org/ericvsmith/namedlist',
       author='Eric V. Smith',
       author_email='eric@trueblade.com',
